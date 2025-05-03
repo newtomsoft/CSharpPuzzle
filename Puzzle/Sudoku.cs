093a392a8c93d844e0d8cd2e949ff8580a43d1a9
Thomas <thomas@newtomsoft.fr>
2025-05-03 18:02:31 +0200

    Refactor Sudoku code to use constants for grid dimensions
    Replaced hardcoded grid and block sizes with constants `GridSize` and `BlockSize` for improved readability and maintainability. Updated all relevant loops and conditions to use these constants consistently. This change simplifies future modifications to the grid dimensions.

--- Puzzle/Sudoku.cs
+++ Puzzle/Sudoku.cs
@@ -4,63 +4,64 @@ namespace Puzzle;
 
 public static class Sudoku
 {
+    private const int GridSize = 9;
+    private const int BlockSize = 3;
     public static int[,] SolveSudoku(int[,] puzzle)
     {
-        const int gridSize = 9;
         using var ctx = new Context();
 
-        var matrix = new IntExpr[9, 9];
-        for (var i = 0; i < 9; i++)
-        for (var j = 0; j < 9; j++)
+        var matrix = new IntExpr[GridSize, GridSize];
+        for (var i = 0; i < GridSize; i++)
+        for (var j = 0; j < GridSize; j++)
             matrix[i, j] = ctx.MkIntConst($"cell_{i}_{j}");
 
         var solver = ctx.MkSolver();
 
-        for (var i = 0; i < 9; i++)
-        for (var j = 0; j < 9; j++)
+        for (var i = 0; i < GridSize; i++)
+        for (var j = 0; j < GridSize; j++)
             solver.Add(ctx.MkAnd(ctx.MkGe(matrix[i, j], ctx.MkInt(1)), ctx.MkLe(matrix[i, j], ctx.MkInt(9))));
 
 
-        for (var rowIndex = 0; rowIndex < gridSize; rowIndex++)
+        for (var rowIndex = 0; rowIndex < GridSize; rowIndex++)
         {
-            Expr[] rowValues = Enumerable.Range(0, gridSize).Select(col => matrix[rowIndex, col]).ToArray();
+            var rowValues = Enumerable.Range(0, GridSize).Select(col => matrix[rowIndex, col]).ToArray<Expr>();
             solver.Add(ctx.MkDistinct(rowValues));
         }
 
-        for (var columnIndex = 0; columnIndex < gridSize; columnIndex++)
+        for (var columnIndex = 0; columnIndex < GridSize; columnIndex++)
         {
-            Expr[] columnValues = Enumerable.Range(0, gridSize).Select(i => matrix[i, columnIndex]).ToArray();
+            var columnValues = Enumerable.Range(0, GridSize).Select(i => matrix[i, columnIndex]).ToArray<Expr>();
             solver.Add(ctx.MkDistinct(columnValues));
         }
 
-        for (var blockRow = 0; blockRow < 3; blockRow++)
+        for (var blockRow = 0; blockRow < BlockSize; blockRow++)
         {
-            for (var blockCol = 0; blockCol < 3; blockCol++)
+            for (var blockCol = 0; blockCol < BlockSize; blockCol++)
             {
-                var blockValues = new Expr[gridSize];
+                var blockValues = new Expr[GridSize];
                 var index = 0;
 
-                for (var i = 0; i < 3; i++)
-                for (var j = 0; j < 3; j++)
+                for (var i = 0; i < BlockSize; i++)
+                for (var j = 0; j < BlockSize; j++)
                     blockValues[index++] = matrix[blockRow * 3 + i, blockCol * 3 + j];
 
                 solver.Add(ctx.MkDistinct(blockValues));
             }
         }
 
-        for (var i = 0; i < 9; i++)
-        for (var j = 0; j < 9; j++)
+        for (var i = 0; i < GridSize; i++)
+        for (var j = 0; j < GridSize; j++)
             if (puzzle[i, j] != 0)
                 solver.Add(ctx.MkEq(matrix[i, j], ctx.MkInt(puzzle[i, j])));
 
         var status = solver.Check();
-        if (status != Status.SATISFIABLE) return new int[9, 9];
+        if (status != Status.SATISFIABLE) return new int[GridSize, GridSize];
         {
             var model = solver.Model;
-            var solution = new int[9, 9];
+            var solution = new int[GridSize, GridSize];
 
-            for (var i = 0; i < 9; i++)
-            for (var j = 0; j < 9; j++)
+            for (var i = 0; i < GridSize; i++)
+            for (var j = 0; j < GridSize; j++)
                 solution[i, j] = ((IntNum)model.Evaluate(matrix[i, j])).Int;
 
             return solution;
@@ -69,12 +70,12 @@ public static class Sudoku
 
     public static void PrintSudoku(int[,] grid)
     {
-        for (var i = 0; i < 9; i++)
+        for (var i = 0; i < GridSize; i++)
         {
-            if (i % 3 == 0 && i != 0) Console.WriteLine("------+-------+------");
-            for (var j = 0; j < 9; j++)
+            if (i % BlockSize == 0 && i != 0) Console.WriteLine("------+-------+------");
+            for (var j = 0; j < GridSize; j++)
             {
-                if (j % 3 == 0 && j != 0) Console.Write("| ");
+                if (j % BlockSize == 0 && j != 0) Console.Write("| ");
                 var value = grid[i, j];
                 if (value == 0) Console.Write(". ");
                 else Console.Write(value + " ");
